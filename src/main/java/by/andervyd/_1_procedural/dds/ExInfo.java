bb52b40141a99b31dca8deac795864f1bb5ed50a
andervyd <andervyd@gmail.com>
2021-06-04 21:26:16 +0300

    added Lecture 4 Part 4

---
+++ src/main/java/by/andervyd/_1_procedural/dds/ExInfo.java
@@ -0,0 +1,20 @@
+package by.andervyd._1_procedural.dds;
+
+public class ExInfo {
+    public static void main(String[] args) {
+
+        Node tail = Node.genRecFull(10, 20, 30, 40);
+
+        System.out.println(Node.toStringRec(tail));
+        // >> 10 -> 20 -> 30 -> 40 -> *
+
+        System.out.println("Length tail: " + Node.length(tail));
+        // >> Length tail: 4
+
+        System.out.println("Max value tail: " + Node.max(tail));
+        // >> Max value tail: 40
+
+        System.out.println("Sum tail: " + Node.sum(tail));
+        // >> Sum tail: 100
+    }
+}
--- src/main/java/by/andervyd/_1_procedural/dds/Node.java
+++ src/main/java/by/andervyd/_1_procedural/dds/Node.java
@@ -61,6 +61,19 @@ public class Node {
         }
     }
 
+    public static int length(Node tail) {
+        return (tail == null) ? 0 : 1 + length(tail.next);
+
+    }
+
+    public static int max(Node tail) {
+        return (tail == null) ? 0 : Math.max(tail.value, max(tail.next));
+    }
+
+    public static int sum(Node tail) {
+        return (tail == null) ? 0 : tail.value + sum(tail.next);
+    }
+
     public static String toStringIter(Node tail) {
         String result = "";
         while (tail != null) {
